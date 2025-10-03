2adc740498aecb9e04d755fa5fa682b6f38c498f
Damian Dan <dan.damian@isa.utm.md>
2025-10-03 20:10:48 +0300

    Initial flutter commit

--- CandleCore.Web/lib/routes.dart
+++
@@ -1,6 +0,0 @@
-import 'package:candle_core/presentation/screens/home_page.dart';
-import 'package:flutter/material.dart';
-
-Map<String, WidgetBuilder> getRoutes() {
-  return {'/': (context) => const HomePage()};
-}
